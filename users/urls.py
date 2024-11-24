4bc9291a819ac43ae6930c09fbdc30356b621f11
Riddhub <unkownrid@gmail.com>
2024-11-24 13:06:30 +0300

    - add profile

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,19 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 
-from users.views import logout_view, UserRegisterView
+from users.views import (
+    logout_view,
+    UserRegisterView,
+    UserProfileView,
+    UserLoginView,
+)
 
 app_name = 'users'
 
 urlpatterns = [
     path('logout/', logout_view, name='logout'),
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    # path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('registration/', UserRegisterView.as_view(), name='registration'),
+    path('<int:pk>/profile/', UserProfileView.as_view(), name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,10 @@
 from django.contrib.auth import logout
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, TemplateView
 
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserProfileForm, UserLoginForm
 from users.models import User
 
 
@@ -14,8 +13,22 @@ def logout_view(request):
     return redirect('products:index')
 
 
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+    success_url = reverse_lazy('products:index')
+
+
 class UserRegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/registration.html'
     success_url = reverse_lazy('products:index')
     success_message = 'Вы успешно зарегистрировались. Можете войти на сайт!'
+
+
+class UserProfileView(UpdateView):
+    form_class = UserProfileForm
+    queryset = User.objects.all()
+    template_name = 'users/profile.html'
+    success_url = reverse_lazy('products:index')
+
