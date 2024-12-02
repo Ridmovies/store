8a34d205b8ed986dd1ca535d344ca3a6939eff5a
riddler_rid <unkownrid@gmail.com>
2024-12-02 13:45:45 +0300

    create services.py in orders app

---
+++ orders/services.py
@@ -0,0 +1,15 @@
+from orders.models import Order
+from products.models import Basket
+from products.services import get_total_sum
+
+
+def update_success_order(user, order_id) -> None:
+    order = Order.objects.get(id=order_id)
+    baskets = Basket.objects.filter(user=user)
+    order.basket_history = {
+        'purchased_items': [basket.de_json() for basket in baskets],
+        'total_sum': float(get_total_sum(baskets)),
+    }
+    baskets.delete()
+    order.status = 1
+    order.save()
--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,7 @@ from django.views.generic import CreateView, TemplateView, ListView, DetailView
 from common.mixins import TitleMixin
 from orders.forms import OrderForm
 from orders.models import Order
+from orders.services import update_success_order
 from orders.yookassa_services import get_confirmation_url, get_payment_info
 from products.models import Basket
 from products.services import get_total_sum
@@ -28,27 +29,16 @@ def order_payment(request):
     value: str = str(get_total_sum(baskets))
     description = order.id
     url, payment_id = get_confirmation_url(order.id, value, description)
-    print(f"{payment_id=}")
     return redirect(url)
 
 
 def check_payment(request, payment_id: str):
     payment_info = get_payment_info(payment_id)
     order_id = payment_info.metadata.get("orderId")
-    order = Order.objects.get(id=order_id)
-    print(f"{payment_info.status=}")
-    if payment_info.status == "succeeded":
-        baskets = Basket.objects.filter(user=request.user)
 
-        order.basket_history = {
-        'purchased_items': [basket.de_json() for basket in baskets],
-        'total_sum': float(get_total_sum(baskets)),
-        }
-        baskets.delete()
-        order.status = 1
-        order.save()
-    else:
-        pass
+    if payment_info.status == "succeeded":
+        # Обновляем заказ
+        update_success_order(request.user, order_id)
     return redirect("products:index")
 
 
